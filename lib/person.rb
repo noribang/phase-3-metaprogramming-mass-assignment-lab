dc4e8d83097e365a39ed59388dacad96c0b6cb5b
hoonkim <hoonk9387@gmail.com>
2022-07-28 21:22:43 -0500

    Completed assignment. Used keyword arguments and mass assignment.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,12 @@
 class Person
   # your code here
+  def initialize(attributes)
+    attributes.each do |key, value|
+      # create a getter and setter by calling the attr_accessor method 
+      self.class.attr_accessor(key)
+      # send will set value to appropriate setter method
+      self.send("#{key}=", value)
+    end
+
+  end
 end
